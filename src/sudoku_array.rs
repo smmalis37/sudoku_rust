621895e730023ffd6eedaf7030c16a2565ae72ee
Steven Malis <smmalis37@gmail.com>
2021-01-23 14:31:53 -0800

    Relax bounds on SudokuArray, kinda.

--- src/sudoku_array.rs
+++ src/sudoku_array.rs
@@ -3,19 +3,21 @@ use druid::Data;
 use std::ops::{Index, IndexMut};
 
 #[derive(Clone, Data)]
-pub struct SudokuArray<T: Clone + Data>([T; SIZE2]);
+pub struct SudokuArray<T>([T; SIZE2]);
 
 impl<T: Copy + Clone + Data> SudokuArray<T> {
     pub fn new(val: T) -> Self {
         Self([val; SIZE2])
     }
+}
 
+impl<T> SudokuArray<T> {
     pub fn enumerate(&self) -> impl Iterator<Item = (Num, &T)> {
         (1..).zip(self.0.iter())
     }
 }
 
-impl<T: Clone + Data> Index<Num> for SudokuArray<T> {
+impl<T> Index<Num> for SudokuArray<T> {
     type Output = T;
 
     fn index(&self, index: Num) -> &Self::Output {
@@ -23,7 +25,7 @@ impl<T: Clone + Data> Index<Num> for SudokuArray<T> {
     }
 }
 
-impl<T: Clone + Data> Index<usize> for SudokuArray<T> {
+impl<T> Index<usize> for SudokuArray<T> {
     type Output = T;
 
     fn index(&self, index: usize) -> &Self::Output {
@@ -31,13 +33,13 @@ impl<T: Clone + Data> Index<usize> for SudokuArray<T> {
     }
 }
 
-impl<T: Clone + Data> IndexMut<Num> for SudokuArray<T> {
+impl<T> IndexMut<Num> for SudokuArray<T> {
     fn index_mut(&mut self, index: Num) -> &mut Self::Output {
         &mut self[index as usize]
     }
 }
 
-impl<T: Clone + Data> IndexMut<usize> for SudokuArray<T> {
+impl<T> IndexMut<usize> for SudokuArray<T> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.0[index - 1]
     }
